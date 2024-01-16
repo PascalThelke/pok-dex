7ecbe9bbd6ac8a0fed90b579a33f417a9bfed119
Pascal Thelke <p.thelke@googlemail.com>
2024-01-16 22:53:20 +0100

    pokedex header + some css

--- script.js
+++ script.js
@@ -16,7 +16,22 @@ function init(){
 
 function renderPokemonInfo(){
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonPicture').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
-
+    capitalizeFirstLetter('pokemonName');
+    document.getElementById('firstType').innerHTML = currentPokemon['types']['0']['type']['name'];
+    capitalizeFirstLetter('firstType');
+    document.getElementById('secondType').innerHTML = currentPokemon['types']['1']['type']['name'];
+    capitalizeFirstLetter('secondType');
+    document.getElementById('pokemonPicture').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+    document.getElementById('pokemonId').innerHTML = '#00' + currentPokemon['id'];
 
 }
+
+function capitalizeFirstLetter(elementId) {
+    let element = document.getElementById(elementId);
+    if (element) {
+      let text = element.textContent;
+      let capitalizedText = text.charAt(0).toUpperCase() + text.slice(1);
+      element.textContent = capitalizedText;
+    }
+  }
+
